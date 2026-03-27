fdfcf684dd361a96b59597695a686bfd3c9358b3
Pie Boris <pieboris21@gmail.com>
2026-03-27 11:56:06 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   // ⚡ Activer CORS pour le frontend
   app.enableCors({
-    origin: 'http://localhost:3001', // ton frontend
+    origin: 'https://objecte-manager-frontend.vercel.app/', // ton frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -23,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
